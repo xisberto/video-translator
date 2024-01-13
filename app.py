04e6f2e5b37929cc53aeaf1ebd8b1a050bb993dd
Humberto Fraga <xisberto@gmail.com>
2024-01-13 11:58:20 -0300

    Corrige parâmetros de input e output do translate

--- app.py
+++ app.py
@@ -21,6 +21,7 @@ role_arn = os.getenv('VIDEO_ROLE_ARN')
 def handle_new_video(event: S3Event):
     """Detecta novo arquivo mp4 no bucket de origem e inicia os trabalhos de Rekognition e Transcribe"""
     timestamp = datetime.datetime.now().strftime("%Y-%m-%dT%H.%M.%S")
+    filename = re.compile(r"(.*).mp4").match(event.key).groups()[0]
     rek_client = boto3.client('rekognition')
     rek_client.start_label_detection(
         Video={
@@ -34,11 +35,11 @@ def handle_new_video(event: S3Event):
             'SNSTopicArn': sns_topic,
             'RoleArn': role_arn
         },
-        JobTag=f"{timestamp}__{event.bucket}__{event.key}"
+        JobTag=f"{timestamp}__{event.bucket}__{filename}"
     )
     transc_client = boto3.client('transcribe')
     transc_client.start_transcription_job(
-        TranscriptionJobName=f"{timestamp}__{event.bucket}__{event.key}",
+        TranscriptionJobName=f"{timestamp}__{filename}",
         MediaFormat="mp4",
         IdentifyLanguage=True,
         Media={
@@ -47,23 +48,29 @@ def handle_new_video(event: S3Event):
         Subtitles={
             'Formats': ['srt']
         },
-        OutputBucketName=destination_bucket
+        OutputBucketName=destination_bucket,
+        OutputKey=f"{timestamp}__{filename}/subtitles/"
     )
 
 
 @app.on_s3_event(bucket=destination_bucket, events=['s3:ObjectCreated:*'], suffix='.srt')
 def handle_transcription(event: S3Event):
     """Detecta o fim do trabalho de Transcribe e inicia o trabalho de tradução"""
-    app.log.debug(f"New transcription detected: {event.bucket}/{event.key}")
+    app.log.debug(f"New transcription detected: {event.key}")
     transl_client = boto3.client('translate')
-    return transl_client.start_text_translation_job(
+    containing_folder = re.compile(r"(.*)/subtitles/.*").match(event.key).groups()[0]
+    input_uri = f"s3://{event.bucket}/{containing_folder}/subtitles"
+    output_uri = f"s3://{destination_bucket}/{containing_folder}/translations"
+    app.log.debug(f"Input: {input_uri}")
+    app.log.debug(f"Output: {output_uri}")
+    transl_client.start_text_translation_job(
         JobName=f"{event.bucket}__{event.key}",
         InputDataConfig={
-            'S3Uri': f"s3://{event.bucket}/{event.key}",
+            'S3Uri': input_uri,
             'ContentType': "text/plain"
         },
         OutputDataConfig={
-            'S3Uri': f"s3://{destination_bucket}/"
+            'S3Uri': output_uri
         },
         DataAccessRoleArn=role_arn,
         SourceLanguageCode="pt",
@@ -76,7 +83,7 @@ def handle_video_label_detected(event: SNSEvent):
     message = json.loads(event.message)
     app.log.debug(event.message)
     bucket = message['Video']['S3Bucket']
-    filename = message['Video']['S3ObjectName']
+    filename = re.compile(r"(.*).mp4").match(message['Video']['S3ObjectName']).groups()[0]
     app.log.debug(f"New SNS message for {bucket}/{filename}")
     rek_client = boto3.client('rekognition')
     job = rek_client.get_label_detection(
@@ -85,7 +92,7 @@ def handle_video_label_detected(event: SNSEvent):
     )
     job_tag = job['JobTag']
     timestamp = re.compile(r"(.*)__.*__.*").match(job_tag).groups()[0]
-    object_key = f"{timestamp}__{bucket}__{filename}-labels.json"
+    object_key = f"{timestamp}__{filename}/labels.json"
     s3_resource = boto3.resource('s3')
     bucket = s3_resource.Bucket(destination_bucket)
     bucket.put_object(Key=object_key, Body=json.dumps(job))
